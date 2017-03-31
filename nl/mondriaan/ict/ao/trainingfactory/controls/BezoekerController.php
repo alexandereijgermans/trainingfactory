a0094b27ba7c6ca1561c4a0108cd34c8e8adfac0
MikeMelein <m.melein@mondriaanict.nl>
2017-03-31 12:56:34 +0200

    training aanbod

--- nl/mondriaan/ict/ao/trainingfactory/controls/BezoekerController.php
+++ nl/mondriaan/ict/ao/trainingfactory/controls/BezoekerController.php
@@ -40,4 +40,37 @@ class BezoekerController extends \ao\php\framework\controls\AbstractController
 //        $directeur = $this->model->getDirecteur();
 //        $this->view->set("directeur",$directeur);
     }
+    
+    public function aanbodAction()
+    {
+        if($this->model->isPostLeeg())
+        {
+           $this->view->set("boodschap","Vul uw gegevens in");
+        }
+        else
+        {   
+            $resultInlog=$this->model->controleerInloggen();
+            switch($resultInlog)
+            {
+                case REQUEST_SUCCESS:
+                     $this->view->set("boodschap","Welkom op de beheers applicatie. Veel werkplezier");
+                     $recht = $this->model->getGebruiker()->getRole();
+                     $this->forward("default", $recht);
+                     break;
+                case REQUEST_FAILURE_DATA_INVALID:
+                     $this->view->set("boodschap","Gegevens kloppen niet. Probeer opnieuw."); 
+
+                     break;
+                case REQUEST_FAILURE_DATA_INCOMPLETE:
+                    $this->forward("default", "bezoeker");
+                     break;
+                case REQUEST_FAILURE_DATA_INCOMPLETE:
+                    $this->forward("default", "bezoeker");
+                     $this->view->set("boodschap","niet alle gegevens ingevuld");
+                     break;
+            }
+        }
+        $training=$this->model->getTraining();
+        $this->view->set("training",$training);
+    }
 }
--- nl/mondriaan/ict/ao/trainingfactory/models/db/Training.php
+++ nl/mondriaan/ict/ao/trainingfactory/models/db/Training.php
@@ -3,15 +3,32 @@ namespace nl\mondriaan\ict\ao\trainingfactory\models\db;
 class Training 
 {
     private $id;
+    private $description;
+    private $duration;
+    private $extra_costs;
     
     public function __construct()
     {
         $this->id = filter_var($this->id,FILTER_VALIDATE_INT);
-//        $this->afdelings_id = filter_var($this->afdelings_id,FILTER_VALIDATE_INT);
     }
     
     public function getId()
     {
         return $this->id;
+    }
+    
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+    
+    public function getExtraCosts()
+    {
+        return $this->extra_costs;
     }
 }
