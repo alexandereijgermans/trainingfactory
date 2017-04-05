56e994691f8ae7a2bdc7105055be988ef7dbe23e
MikeMelein <m.melein@mondriaanict.nl>
2017-04-05 14:02:01 +0200

    Menu's and Footers

--- nl/mondriaan/ict/ao/trainingfactory/view/tpls/instructor/include/menu.php
+++ nl/mondriaan/ict/ao/trainingfactory/view/tpls/instructor/include/menu.php
@@ -1,1 +1,17 @@
-
+<nav>
+    <ul>
+        <li>
+            <a href="?control=instructor&action=default">Home</a>
+        </li>
+        <li>
+            <a href="#">Plannen Lessen</a>
+        </li>
+        <li>
+            <a href="#">Lessen Beheer</a>
+        </li>
+    </ul>
+</nav>
+<section class="menupicborder">
+    <img class="menupic" src="img/default.jpg"/>
+    <img class="menupic" src="img/default.jpg"/>  
+    <img class="menupic" src="img/default.jpg"/>
