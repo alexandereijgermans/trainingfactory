c5a238923d140b6b383f07f90ea96b182a9d791e
Cas van Vastenburg <c.vastenburg@mondriaanict.nl>
2017-04-13 11:10:48 +0200

    instructor pary 1

--- nl/mondriaan/ict/ao/trainingfactory/controls/InstructorController.php
+++ nl/mondriaan/ict/ao/trainingfactory/controls/InstructorController.php
@@ -17,4 +17,34 @@ class InstructorController extends \ao\php\framework\controls\AbstractController
 //       $gebruiker = $this->model->getGebruiker();
 //       $this->view->set("gebruiker",$gebruiker);
     }
+    
+    public function lesPlannenAction()
+    {
+        $trainingen = $this->model->getTrainingen();
+        $this->view->set('trainingen', $trainingen);
+        if($this->model->isPostLeeg())
+        {
+           $this->view->set("boodschap","Vul uw gegevens in");
+        }
+        else
+        {   
+            $result=$this->model->lesPlannen();
+
+            switch($result)
+            {
+                case REQUEST_SUCCESS:
+                     $this->view->set("boodschap","U hebt een les toegevoegd!");                     
+                     $this->forward("default");
+                     break;
+                case REQUEST_FAILURE_DATA_INVALID:
+                     $this->view->set('form_data',$_POST);
+                     $this->view->set("boodschap","Er is wat fout gegaan, probeer het opnieuw"); 
+                     break;
+                case REQUEST_FAILURE_DATA_INCOMPLETE:
+                     $this->view->set('form_data',$_POST);
+                     $this->view->set("boodschap","Niet alle gegevens ingevuld");
+                     break;
+            }
+        }    
+    }
 }
--- nl/mondriaan/ict/ao/trainingfactory/models/InstructorModel.php
+++ nl/mondriaan/ict/ao/trainingfactory/models/InstructorModel.php
@@ -28,4 +28,49 @@ class InstructorModel extends \ao\php\framework\models\AbstractModel
        $_SESSION = array();
        session_destroy();
    }
+   
+    public function getTrainingen() {
+        $sql = 'SELECT * FROM `training`';
+        $stmnt = $this->dbh->prepare($sql);
+        $stmnt->execute();
+        $trainingen = $stmnt->fetchAll(\PDO::FETCH_CLASS, __NAMESPACE__.'\db\Training');
+        return $trainingen;
+    }
+   
+   public function lesPlannen()
+   {
+       $person_id = $this->getGebruiker()->getId();
+        $time = filter_input(INPUT_POST, 'time');
+        $date = filter_input(INPUT_POST, 'date');
+        $location = filter_input(INPUT_POST, 'location');
+        $max_persons = filter_input(INPUT_POST, 'max_persons');
+        $training = filter_input(INPUT_POST, 'training');
+        
+        if($time === null || $date === null || $location === null || $max_persons === null || $training === null || $person_id === null) {
+            return REQUEST_FAILURE_DATA_INCOMPLETE;
+        }
+        
+        $sql="INSERT INTO `lesson` ( time, date, location, max_persons,person_id, training_id) VALUES( :time, :date, :location, :max_persons,:person_id, :training)";
+
+        $stmnt = $this->dbh->prepare($sql);
+        $stmnt->bindParam(':time', $time);
+        $stmnt->bindParam(':date', $date);
+        $stmnt->bindParam(':location', $location);
+        $stmnt->bindParam(':max_persons', $max_persons);
+        $stmnt->bindParam(':person_id', $person_id);
+        $stmnt->bindParam('training', $training);
+
+
+        try {
+            $stmnt->execute();
+        } catch (\PDOException $e) {
+            return REQUEST_FAILURE_DATA_INVALID;
+        }
+
+        if($stmnt->rowCount() === 1) {
+
+            return REQUEST_SUCCESS;
+        }
+        return REQUEST_FAILURE_DATA_INVALID;
+   }
 }
